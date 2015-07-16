1bb2a6ff76d4cd884e6d710b379112d5e315a1e9
benji6 <benhall22@hotmail.co.uk>
2015-07-16 19:51:00 +0100

    update src for scheduling oscillator start and stop times

--- src/tools/createAudioNode.js
+++ src/tools/createAudioNode.js
@@ -4,13 +4,14 @@ const namesToParamsKey = {
   delay: 'maxDelayTime',
 };
 
-module.exports = (audioContext, name, constructorParams) => {
+module.exports = (audioContext, name, constructorParams, {startTime, stopTime}) => {
   const constructorParamsKey = namesToParamsKey[name];
   const audioNode = constructorParamsKey ?
     audioContext[`create${capitalize(name)}`](constructorParams[constructorParamsKey]) :
     audioContext[`create${capitalize(name)}`]();
   if (name === 'oscillator') {
-    audioNode.start();
+    audioNode.start(startTime);
+    audioNode.stop(stopTime);
   }
   return audioNode;
 };
--- src/virtualNodeConstructors/NativeVirtualAudioNode.js
+++ src/virtualNodeConstructors/NativeVirtualAudioNode.js
@@ -24,8 +24,9 @@ module.exports = class NativeVirtualAudioNode {
   constructor (virtualAudioGraph, virtualNodeParams) {
     let {node, id, input, output, params} = virtualNodeParams;
     params = params || {};
+    const {startTime, stopTime} = params;
     const constructorParams = pick(constructorParamsKeys, params);
-    this.audioNode = createAudioNode(virtualAudioGraph.audioContext, node, constructorParams);
+    this.audioNode = createAudioNode(virtualAudioGraph.audioContext, node, constructorParams, {startTime, stopTime});
     this.connected = false;
     this.node = node;
     this.updateAudioNode(params);
