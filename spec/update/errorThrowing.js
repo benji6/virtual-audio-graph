9753ebae740e6c74cc5c756761b916cf1fe05ab5
benji6 <benhall22@hotmail.co.uk>
2015-07-25 09:35:21 +0100

    update spec for new api

--- spec/update/errorThrowing.js
+++ spec/update/errorThrowing.js
@@ -15,30 +15,33 @@ describe('virtualAudioGraph.update - error throwing conditions', function () {
 
   it('throws an error if no output is provided', function () {
     expect(function () {
-      virtualAudioGraph.update([{
-        node: 'gain',
-        id: 0,
-      }]);
+      virtualAudioGraph.update({
+        0: {
+          node: 'gain',
+        },
+      });
     }).toThrow();
   });
 
   it('throws an error when virtual node name property is not recognised', function () {
     expect(function () {
-      virtualAudioGraph.update([{
-        id: 0,
-        node: 'foobar',
-        output: 'output',
-      }]);
+      virtualAudioGraph.update({
+        0: {
+          node: 'foobar',
+          output: 'output',
+        },
+      });
     }).toThrow();
   });
 
   it('throws an error when id is "output"', function () {
     expect(function () {
-      virtualAudioGraph.update([{
-        id: 'output',
-        node: 'gain',
-        output: 'output',
-      }]);
+      virtualAudioGraph.update({
+          output: {
+          node: 'gain',
+          output: 'output',
+        },
+      });
     }).toThrow();
   });
 });
