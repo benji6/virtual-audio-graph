8a48109429c2fe445c8effca435924fe77b0a024
benji6 <benhall22@hotmail.co.uk>
2015-07-26 12:36:03 +0100

    disconnect for custom nodes

--- src/tools/disconnect.js
+++ src/tools/disconnect.js
@@ -1,9 +1,9 @@
-const {forEach} = require('ramda');
+const {forEach, values} = require('ramda');
 
 // VirtualNode -> nil
 module.exports = function disconnect (virtualNode) {
   if (virtualNode.isCustomVirtualNode) {
-    forEach((childVirtualNode) => disconnect(childVirtualNode), virtualNode.virtualNodes);
+    forEach(childVirtualNode => disconnect(childVirtualNode), values(virtualNode.virtualNodes));
   } else {
     virtualNode.audioNode.disconnect();
   }
