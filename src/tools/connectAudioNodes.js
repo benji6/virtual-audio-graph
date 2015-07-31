c9638858acf531082fff47eab9ef349ec12bda71
benji6 <benhall22@hotmail.co.uk>
2015-07-31 22:28:39 +0100

    Revert "handle case where property is undefined"
    This reverts commit f7c47bf65eaf1dba36ef1e3972036327e4146fc4.

--- src/tools/connectAudioNodes.js
+++ src/tools/connectAudioNodes.js
@@ -8,7 +8,7 @@ const isPlainOldObject = x => equals(Object.prototype.toString.call(x),
 module.exports = (virtualGraph, handleConnectionToOutput = () => {}) =>
   forEach(id => {
             const virtualNode = virtualGraph[id];
-            if (isNil(virtualNode) || virtualNode.connected) {
+            if (virtualNode.connected) {
               return;
             }
             forEach(output => {
