5db14745851cb2242f493119bb821587d78f90b5
benji6 <benhall22@hotmail.co.uk>
2015-11-21 13:00:13 +0000

    update build

--- dist/data/audioParamProperties.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, '__esModule', {
-  value: true
-});
-exports['default'] = ['attack', 'delayTime', 'detune', 'frequency', 'gain', 'knee', 'pan', 'playbackRate', 'ratio', 'reduction', 'release', 'threshold', 'Q'];
-module.exports = exports['default'];
--- dist/data/constructorParamsKeys.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, '__esModule', {
-  value: true
-});
-exports['default'] = ['maxDelayTime', 'mediaElement', 'mediaStream', 'numberOfOutputs'];
-module.exports = exports['default'];
