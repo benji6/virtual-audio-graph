811b0483ce05b1bb35cd041b02ad564f2c264b47
benji6 <benhall22@hotmail.co.uk>
2016-02-17 17:40:59 +0000

    style(createStandardVirtualAudioNode)

--- src/virtualNodeFactories/createStandardVirtualAudioNode.js
+++ src/virtualNodeFactories/createStandardVirtualAudioNode.js
@@ -18,14 +18,8 @@ const createAudioNode = (audioContext, name, constructorParam, {startTime, stopT
     ? audioContext[`create${capitalize(name)}`](constructorParam)
     : audioContext[`create${capitalize(name)}`]()
   if (startAndStopNodes.indexOf(name) !== -1) {
-    if (startTime == null) {
-      audioNode.start()
-    } else {
-      audioNode.start(startTime)
-    }
-    if (stopTime != null) {
-      audioNode.stop(stopTime)
-    }
+    if (startTime == null) audioNode.start(); else audioNode.start(startTime)
+    if (stopTime != null) audioNode.stop(stopTime)
   }
   return audioNode
 }
@@ -38,43 +32,34 @@ const disconnect = function () {
 
 const disconnectAndDestroy = function () {
   const {audioNode, stopCalled} = this
-  if (audioNode.stop && !stopCalled) {
-    audioNode.stop()
-  }
+  if (audioNode.stop && !stopCalled) audioNode.stop()
   audioNode.disconnect && audioNode.disconnect()
   this.connected = false
 }
 
 const update = function (params = {}) {
-  Object.keys(params)
-    .forEach(key => {
-      if (constructorParamsKeys.indexOf(key) !== -1) {
-        return
-      }
-      const param = params[key]
-      if (this.params && this.params[key] === param) {
-        return
-      }
-      if (audioParamProperties.indexOf(key) !== -1) {
-        if (Array.isArray(param)) {
-          if (this.params && !deepEqual(param, this.params[key], {strict: true})) {
-            this.audioNode[key].cancelScheduledValues(0)
-          }
-          const callMethod = ([methodName, ...args]) => this.audioNode[key][methodName](...args)
-          Array.isArray(param[0])
-            ? param.forEach(callMethod)
-            : callMethod(param)
-          return
+  Object.keys(params).forEach(key => {
+    if (constructorParamsKeys.indexOf(key) !== -1) return
+    const param = params[key]
+    if (this.params && this.params[key] === param) return
+    if (audioParamProperties.indexOf(key) !== -1) {
+      if (Array.isArray(param)) {
+        if (this.params && !deepEqual(param, this.params[key], {strict: true})) {
+          this.audioNode[key].cancelScheduledValues(0)
         }
-        this.audioNode[key].value = param
+        const callMethod = ([methodName, ...args]) => this.audioNode[key][methodName](...args)
+        Array.isArray(param[0]) ? param.forEach(callMethod) : callMethod(param)
         return
       }
-      if (setters.indexOf(key) !== -1) {
-        this.audioNode[`set${capitalize(key)}`](...param)
-        return
-      }
-      this.audioNode[key] = param
-    })
+      this.audioNode[key].value = param
+      return
+    }
+    if (setters.indexOf(key) !== -1) {
+      this.audioNode[`set${capitalize(key)}`](...param)
+      return
+    }
+    this.audioNode[key] = param
+  })
   this.params = params
   return this
 }
