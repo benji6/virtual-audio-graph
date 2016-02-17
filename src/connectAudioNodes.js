0c44040f727f274d76a04e8f1e393515c80af45c
benji6 <benhall22@hotmail.co.uk>
2016-02-17 08:42:44 +0000

    style(connectAudioNodes)

--- src/connectAudioNodes.js
+++ src/connectAudioNodes.js
@@ -4,39 +4,34 @@ export default (virtualGraph, handleConnectionToOutput = () => {}) =>
   Object.keys(virtualGraph).forEach(id => {
     const virtualNode = virtualGraph[id]
     const {output} = virtualNode
-    if (virtualNode.connected || output == null) {
-      return
-    }
-    asArray(output)
-      .forEach(output => {
-        if (output === 'output') {
-          return handleConnectionToOutput(virtualNode)
+    if (virtualNode.connected || output == null) return
+    asArray(output).forEach(output => {
+      if (output === 'output') return handleConnectionToOutput(virtualNode)
+
+      if (Object.prototype.toString.call(output) === '[object Object]') {
+        const {key, destination, inputs, outputs} = output
+
+        if (key == null) {
+          throw new Error(`id: ${id} - output object requires a key property`)
         }
-
-        if (Object.prototype.toString.call(output) === '[object Object]') {
-          const {key, destination, inputs, outputs} = output
-
-          if (key == null) {
-            throw new Error(`id: ${id} - output object requires a key property`)
+        if (inputs) {
+          if (inputs.length !== outputs.length) {
+            throw new Error(`id: ${id} - outputs and inputs arrays are not the same length`)
           }
-          if (inputs) {
-            if (inputs.length !== outputs.length) {
-              throw new Error(`id: ${id} - outputs and inputs arrays are not the same length`)
-            }
-            return inputs.forEach((input, i) => virtualNode
-              .connect(virtualGraph[key].audioNode, outputs[i], input))
-          }
-          return virtualNode.connect(virtualGraph[key].audioNode[destination])
+          return inputs.forEach((input, i) => virtualNode
+            .connect(virtualGraph[key].audioNode, outputs[i], input))
         }
+        return virtualNode.connect(virtualGraph[key].audioNode[destination])
+      }
 
-        const destinationVirtualAudioNode = virtualGraph[output]
+      const destinationVirtualAudioNode = virtualGraph[output]
 
-        if (destinationVirtualAudioNode.isCustomVirtualNode) {
-          return values(destinationVirtualAudioNode.virtualNodes)
-            .filter(({input}) => input === 'input')
-            .forEach(node => virtualNode.connect(node.audioNode))
-        }
+      if (destinationVirtualAudioNode.isCustomVirtualNode) {
+        return values(destinationVirtualAudioNode.virtualNodes)
+          .filter(({input}) => input === 'input')
+          .forEach(node => virtualNode.connect(node.audioNode))
+      }
 
-        virtualNode.connect(destinationVirtualAudioNode.audioNode)
-      })
+      virtualNode.connect(destinationVirtualAudioNode.audioNode)
+    })
   })
