74f565d6e2c354550b2ed8ebded4798f89b040ba
Ben Hall <benji2357@gmail.com>
2017-06-24 16:44:06 +0100

    chore(rollup): Use babelrc-rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,11 @@
 import babel from 'rollup-plugin-babel'
+import babelrc from 'babelrc-rollup'
 
 export default {
   dest: 'dist/index.js',
   entry: 'src/index.js',
   format: 'cjs',
   plugins: [
-    babel({
-      babelrc: false,
-      exclude: 'node_modules/**',
-      presets: ['es2015-rollup'],
-    }),
+    babel(babelrc()),
   ],
 }
