2ccbf90008f745e8572189469958a24a84a7d848
Ben Hall <benji2357@gmail.com>
2018-04-29 16:23:37 +0100

    chore(createNode): correct params typing to optional

--- src/createNode.ts
+++ src/createNode.ts
@@ -2,5 +2,5 @@ import CustomVirtualAudioNode from './VirtualAudioNodes/CustomVirtualAudioNode'
 import { CustomVirtualAudioNodeFactory, Output, VirtualAudioNodeParams } from './types'
 
 export default (node: CustomVirtualAudioNodeFactory) =>
-  (output: Output, params: VirtualAudioNodeParams) =>
+  (output: Output, params?: VirtualAudioNodeParams) =>
     new CustomVirtualAudioNode(node, output, params)
