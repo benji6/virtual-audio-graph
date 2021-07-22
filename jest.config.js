0c79fe4dcc2866040a052e80eb12ae40cc116e90
Ben Hall <benji2357@gmail.com>
2021-07-22 09:02:46 +0100

    chore(jest): update config so it watches source and test directories

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: "ts-jest",
-  roots: ["<rootDir>/test"],
+  roots: ["<rootDir>/src", "<rootDir>/test"],
   setupFiles: ["<rootDir>/jest/setup.ts"],
   testPathIgnorePatterns: ["/node_modules/", "/test/utils"],
-  testRegex: "^.+\\.ts$",
+  testRegex: "/test/.+\\.ts$",
 };
