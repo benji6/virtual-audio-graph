benji6/virtual-audio-graph

commit 06bc0fb62041e2993c58a37cb76c591513414426

fix(CustomVirtualAudioNode): handle when startTime and stopTime change on child nodes
See #316 https://github.com/benji6/virtual-audio-graph/issues/316

src/VirtualAudioNodes/CustomVirtualAudioNode.ts
@@ -6,9 +6,10 @@ import {
   Output,
   VirtualAudioNode,
 } from "../types";
-import { mapObj, values } from "../utils";
+import { equals, mapObj, values } from "../utils";
+import VirtualAudioNodeBase from "./VirtualAudioNodeBase";
 
-export default class CustomVirtualAudioNode {
+export default class CustomVirtualAudioNode extends VirtualAudioNodeBase {
   public readonly audioNode: undefined = undefined;
   public connected: boolean = false;
   public params: IVirtualAudioNodeParams;
@@ -19,6 +20,7 @@ export default class CustomVirtualAudioNode {
     public output?: Output,
     params?: IVirtualAudioNodeParams
   ) {
+    super();
     this.params = params || {};
   }
 
@@ -68,14 +70,40 @@ export default class CustomVirtualAudioNode {
     return this;
   }
 
-  public update(params: IVirtualAudioNodeParams = {}): this {
+  public update(
+    params: IVirtualAudioNodeParams = {},
+    audioContext: AudioContext
+  ): this {
     const audioGraphParamsFactoryValues = values(this.node(params));
     const keys = Object.keys(this.virtualNodes);
+
     for (let i = 0; i < keys.length; i++) {
-      const p = audioGraphParamsFactoryValues[i];
-      this.virtualNodes[keys[i]].update(p.params);
+      const key = keys[i];
+      const virtualAudioNode = this.virtualNodes[key];
+      const newVirtualAudioNode = audioGraphParamsFactoryValues[i];
+
+      if (virtualAudioNode.cannotUpdateInPlace(newVirtualAudioNode)) {
+        virtualAudioNode.disconnectAndDestroy();
+        this.disconnectParents(virtualAudioNode);
+        this.virtualNodes[key] = newVirtualAudioNode.initialize(audioContext);
+        continue;
+      }
+
+      virtualAudioNode.update(newVirtualAudioNode.params, audioContext);
+
+      if (!equals(newVirtualAudioNode.output, virtualAudioNode.output)) {
+        virtualAudioNode.disconnect();
+        this.disconnectParents(virtualAudioNode);
+        virtualAudioNode.output = newVirtualAudioNode.output;
+      }
     }
+
+    connectAudioNodes(this.virtualNodes, () => {});
     this.params = params;
     return this;
+  }
+
+  private disconnectParents(vNode: VirtualAudioNode): void {
+    for (const node of values(this.virtualNodes)) node.disconnect(vNode);
   }
 }

src/VirtualAudioNodes/StandardVirtualAudioNode.ts
@@ -13,6 +13,7 @@ import {
 } from "../types";
 import { capitalize, equals, find, values } from "../utils";
 import CustomVirtualAudioNode from "./CustomVirtualAudioNode";
+import VirtualAudioNodeBase from "./VirtualAudioNodeBase";
 
 interface IAudioContextFactoryLookup {
   [_: string]: any;
@@ -41,7 +42,7 @@ const createAudioNode = (
   return audioNode;
 };
 
-export default class StandardVirtualAudioNode {
+export default class StandardVirtualAudioNode extends VirtualAudioNodeBase {
   public audioNode: AudioNode;
   public connected: boolean = false;
   private connections: AudioNode[] = [];
@@ -53,8 +54,19 @@ export default class StandardVirtualAudioNode {
     public params?: IVirtualAudioNodeParams,
     public readonly input?: string
   ) {
+    super();
     const stopTime = params && params.stopTime;
     this.stopCalled = stopTime !== undefined;
+  }
+
+  public cannotUpdateInPlace(newVirtualAudioNode: VirtualAudioNode): boolean {
+    return (
+      super.cannotUpdateInPlace(newVirtualAudioNode) ||
+      (newVirtualAudioNode.params && newVirtualAudioNode.params.startTime) !==
+        (this.params && this.params.startTime) ||
+      (newVirtualAudioNode.params && newVirtualAudioNode.params.stopTime) !==
+        (this.params && this.params.stopTime)
+    );
   }
 
   public connect(...connectArgs: any[]): void {

src/VirtualAudioGraph.ts
@@ -38,13 +38,7 @@ export default class VirtualAudioGraph {
         continue;
       }
 
-      if (
-        (newVirtualAudioNode.params && newVirtualAudioNode.params.startTime) !==
-          (virtualAudioNode.params && virtualAudioNode.params.startTime) ||
-        (newVirtualAudioNode.params && newVirtualAudioNode.params.stopTime) !==
-          (virtualAudioNode.params && virtualAudioNode.params.stopTime) ||
-        newVirtualAudioNode.node !== virtualAudioNode.node
-      ) {
+      if (virtualAudioNode.cannotUpdateInPlace(newVirtualAudioNode)) {
         virtualAudioNode.disconnectAndDestroy();
         this.disconnectParents(virtualAudioNode);
         this.virtualNodes[key] = newVirtualAudioNode.initialize(
@@ -59,7 +53,7 @@ export default class VirtualAudioGraph {
         virtualAudioNode.output = newVirtualAudioNode.output;
       }
 
-      virtualAudioNode.update(newVirtualAudioNode.params);
+      virtualAudioNode.update(newVirtualAudioNode.params, this.audioContext);
     }
 
     connectAudioNodes(this.virtualNodes, (vNode: VirtualAudioNode) =>

src/VirtualAudioNodes/AudioWorkletVirtualAudioNode.ts
@@ -1,12 +1,13 @@
 import { IVirtualAudioNodeParams, Output, VirtualAudioNode } from "../types";
 import { equals, values } from "../utils";
 import CustomVirtualAudioNode from "./CustomVirtualAudioNode";
+import VirtualAudioNodeBase from "./VirtualAudioNodeBase";
 
 interface IWindow {
   AudioWorkletNode?: any;
 }
 
-export default class AudioWorkletVirtualAudioNode {
+export default class AudioWorkletVirtualAudioNode extends VirtualAudioNodeBase {
   public audioNode: AudioNode;
   public connected: boolean = false;
   private connections: AudioNode[] = [];
@@ -16,7 +17,9 @@ export default class AudioWorkletVirtualAudioNode {
     public output?: Output,
     public params?: IVirtualAudioNodeParams,
     public readonly input?: string
-  ) {}
+  ) {
+    super();
+  }
 
   public connect(...connectArgs: any[]): void {
     const { audioNode } = this;

src/VirtualAudioNodes/VirtualAudioNodeBase.ts
@@ -1,0 +1,9 @@
+import { CustomVirtualAudioNodeFactory, VirtualAudioNode } from "../types";
+
+export default abstract class VirtualAudioNodeBase {
+  public readonly node: string | CustomVirtualAudioNodeFactory;
+
+  public cannotUpdateInPlace(newVirtualAudioNode: VirtualAudioNode): boolean {
+    return newVirtualAudioNode.node !== this.node;
+  }
+}

commit 5bbe2a91f858d31c8e26a93b495633bec0beaf57

fix(CustomVirtualAudioNode): do not pointlessly disconnect parent nodes when a child node needs to be replaced
Bug introduced in 06bc0fb62041e2993c58a37cb76c591513414426, should fix https://github.com/benji6/virtual-audio-graph/issues/317

src/VirtualAudioNodes/CustomVirtualAudioNode.ts
@@ -71,9 +71,10 @@ export default class CustomVirtualAudioNode extends VirtualAudioNodeBase {
   }
 
   public update(
-    params: IVirtualAudioNodeParams = {},
+    _params: IVirtualAudioNodeParams | null | undefined,
     audioContext: AudioContext
   ): this {
+    const params = _params ?? {};
     const audioGraphParamsFactoryValues = values(this.node(params));
     const keys = Object.keys(this.virtualNodes);
 
@@ -84,7 +85,6 @@ export default class CustomVirtualAudioNode extends VirtualAudioNodeBase {
 
       if (virtualAudioNode.cannotUpdateInPlace(newVirtualAudioNode)) {
         virtualAudioNode.disconnectAndDestroy();
-        this.disconnectParents(virtualAudioNode);
         this.virtualNodes[key] = newVirtualAudioNode.initialize(audioContext);
         continue;
       }
@@ -93,7 +93,6 @@ export default class CustomVirtualAudioNode extends VirtualAudioNodeBase {
 
       if (!equals(newVirtualAudioNode.output, virtualAudioNode.output)) {
         virtualAudioNode.disconnect();
-        this.disconnectParents(virtualAudioNode);
         virtualAudioNode.output = newVirtualAudioNode.output;
       }
     }
@@ -101,9 +100,5 @@ export default class CustomVirtualAudioNode extends VirtualAudioNodeBase {
     connectAudioNodes(this.virtualNodes, () => {});
     this.params = params;
     return this;
-  }
-
-  private disconnectParents(vNode: VirtualAudioNode): void {
-    for (const node of values(this.virtualNodes)) node.disconnect(vNode);
   }
 }

src/VirtualAudioNodes/StandardVirtualAudioNode.ts
@@ -132,7 +132,8 @@ export default class StandardVirtualAudioNode extends VirtualAudioNodeBase {
     return this;
   }
 
-  public update(params: IVirtualAudioNodeParams = {}): this {
+  public update(_params: IVirtualAudioNodeParams | null | undefined): this {
+    const params = _params ?? {};
     const audioNode: IAudioNodePropertyLookup = this.audioNode;
     for (const key of Object.keys(params)) {
       if (constructorParamsKeys.indexOf(key) !== -1) continue;

test/customNodes/__snapshots__/expectedBehaviour.ts.snap
@@ -891,3 +891,33 @@ Object {
   "name": "AudioDestinationNode",
 }
 `;
+
+exports[`custom nodes updating startTime of nested custom nodes 1`] = `
+Object {
+  "inputs": Array [
+    Object {
+      "gain": Object {
+        "inputs": Array [],
+        "value": 1,
+      },
+      "inputs": Array [
+        Object {
+          "detune": Object {
+            "inputs": Array [],
+            "value": 0,
+          },
+          "frequency": Object {
+            "inputs": Array [],
+            "value": 440,
+          },
+          "inputs": Array [],
+          "name": "OscillatorNode",
+          "type": "sine",
+        },
+      ],
+      "name": "GainNode",
+    },
+  ],
+  "name": "AudioDestinationNode",
+}
+`;

test/customNodes/expectedBehaviour.ts
@@ -1,4 +1,5 @@
 import createVirtualAudioGraph, * as V from "../../src";
+import { oscillator } from "../../src";
 import gainWithNoParams from "../utils/gainWithNoParams";
 import pingPongDelay from "../utils/pingPongDelay";
 import sineOsc from "../utils/sineOsc";
@@ -209,5 +210,19 @@ describe("custom nodes", () => {
       ),
     });
     expect(audioContext.toJSON()).toEqual(expectedData);
+  });
+
+  test("updating startTime of nested custom nodes", () => {
+    const voice = V.createNode(({ startTime }) => ({
+      0: gainWithNoParams("output"),
+      1: oscillator(0, { startTime, stopTime: startTime + 1 }),
+    }));
+
+    virtualAudioGraph.update({ 0: voice("output", { startTime: 1 }) });
+    const initialSnapshot = audioContext.toJSON();
+    expect(initialSnapshot).toMatchSnapshot();
+
+    virtualAudioGraph.update({ 0: voice("output", { startTime: 2 }) });
+    expect(initialSnapshot).toEqual(audioContext.toJSON());
   });
 });