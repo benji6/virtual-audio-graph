f34abcbd65e466cb14a41646dbc3b8e74bceebd7
Ben Hall <benji2357@gmail.com>
2025-01-24 18:54:45 +0000

    docs(audioWorklets): fix broken bit crusher

--- docs/audioWorklets/bitCrusher.js
+++ docs/audioWorklets/bitCrusher.js
@@ -18,15 +18,15 @@ class BitCrusher extends AudioWorkletProcessor {
   }
 
   process([input], [output], parameters) {
-    const bitDepth = parameters.bitDepth;
-    const frequencyReduction = parameters.frequencyReduction;
+    const bitDepth = parameters.bitDepth[0];
+    const frequencyReduction = parameters.frequencyReduction[0];
     for (let channel = 0; channel < input.length; channel++) {
       const inputChannel = input[channel];
       const outputChannel = output[channel];
       for (let i = 0; i < inputChannel.length; ++i) {
-        const step = Math.pow(0.5, bitDepth[i]);
-        this.phase += frequencyReduction[i];
+        this.phase += frequencyReduction;
         if (this.phase >= 1) {
+          const step = Math.pow(0.5, bitDepth);
           this.phase -= 1;
           this.lastSampleValue =
             step * Math.floor(inputChannel[i] / step + 0.5);
